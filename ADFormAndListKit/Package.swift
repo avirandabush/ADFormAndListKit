2eeee131d763f9d2741568fe8713e5d7de48c5d0
aviran <avirandabush@gmail.com>
2025-01-08 22:52:37 +0200

    init project

---
+++ ADFormAndListKit/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ADFormAndListKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ADFormAndListKit",
+            targets: ["ADFormAndListKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ADFormAndListKit"),
+        .testTarget(
+            name: "ADFormAndListKitTests",
+            dependencies: ["ADFormAndListKit"]
+        ),
+    ]
+)
